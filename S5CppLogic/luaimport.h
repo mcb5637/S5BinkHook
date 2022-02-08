184881b41c05a635c933c65df6b2aab17cf91ec9
mcb <mgrbo@web.de>
2022-02-08 19:44:56 +0100

    including luapp

--- S5CppLogic/luaimport.h
+++ S5CppLogic/luaimport.h
@@ -3,9 +3,9 @@
 #define LUA_API __declspec(dllimport)
 
 extern "C" {
-#include "lua/lua.h"
-#include "lua/lauxlib.h"
-#include "lua/lualib.h"
+#include "lua50/lua.h"
+#include "lua50/lauxlib.h"
+#include "lua50/lualib.h"
 }
 
 #define lua_abs_index(L, i) ((i) > 0 || (i) <= LUA_REGISTRYINDEX ? (i) : lua_gettop(L) + (i) + 1)

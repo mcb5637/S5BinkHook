4506dccdd6c261488ea31ee334adafc3ed306269
mcb <mgrbo@web.de>
2023-12-29 21:07:55 +0100

    CLogicProperties defaults

--- S5CppLogic/s5_config.h
+++ S5CppLogic/s5_config.h
@@ -51,7 +51,7 @@ namespace GGL {
 		static inline GGL::DamageClassesHolder** const GlobalObj = reinterpret_cast<GGL::DamageClassesHolder**>(0x85A3DC);
 	};
 
-	class CLogicProperties : public BB::IObject {
+	class CLogicProperties : public BB::IObject { // noted here are default values, loading logic.xml might override stuff
 	public:
 		struct SBuildingUpgradeCategory {
 			shok::UpgradeCategoryId Category;
@@ -99,71 +99,71 @@ namespace GGL {
 			virtual unsigned int GetClassIdentifier() const = 0;
 		};
 
-		int CompensationOnBuildingSale;
+		int CompensationOnBuildingSale = 100;
 		shok::Vector<SBuildingUpgradeCategory> BuildingUpgrades;
 		shok::Vector<SSettlerUpgradeCategory> SettlerUpgrades;
 		shok::Vector<STaxationLevel> TaxationLevels;
 		shok::Vector<STradeResource> TradeResources;
 		shok::Vector<SBlessCategory> BlessCategories; // 18
-		float BuildingUnderConstructionExplorationFactor, BuildingPlacementSnapDistance, BuildingClosedHealthFactor; // 22
-		float WeatherExplorationBuildingSnowFactor, WeatherExplorationSettlerSnowFactor; // 25
-		float WeatherMoveSpeedSnowFactor, WeatherExplorationBuildingRainFactor, WeatherExplorationSettlerRainFactor;
-		float WeatherMoveSpeedRainFactor; // 30
-		int WeatherMissChanceChangeRain, WeatherMissChanceChangeSnow;
-		float AttackMoveRange; // 33
-		int TaxAmount;
-		float TaxPenalty;
-		int InitialTaxLevel; // 36
+		float BuildingUnderConstructionExplorationFactor = 1.0f, BuildingPlacementSnapDistance = 100.0f, BuildingClosedHealthFactor; // 22
+		float WeatherExplorationBuildingSnowFactor = 1.0f, WeatherExplorationSettlerSnowFactor = 1.0f; // 25
+		float WeatherMoveSpeedSnowFactor = 1.0f, WeatherExplorationBuildingRainFactor = 1.0f, WeatherExplorationSettlerRainFactor = 1.0f;
+		float WeatherMoveSpeedRainFactor = 1.0f; // 30
+		int WeatherMissChanceChangeRain = 0, WeatherMissChanceChangeSnow = 0;
+		float AttackMoveRange = 1.0f; // 33
+		int TaxAmount = 0;
+		float TaxPenalty = 0;
+		int InitialTaxLevel = 0; // 36
 		shok::CostInfo BlessingCost; // per settler cost
-		float BlessingBonus; // 55
+		float BlessingBonus = 0; // 55
 		PADDINGI(1);
-		int ForceToWorkPenalty;
+		int ForceToWorkPenalty = 0;
 		PADDINGI(1);
-		int ResourceDoodadWarnAmount; // 59
-		int FeedbackHandlerUpdateFrequency; // 60
-		int FeedbackHandlerTimeStaysInSystem;
-		float MilitaryLeaderAutoAttackRange;
-		float MilitaryBuildingAutoAttackRangeFactor;
-		float MilitaryCivilianAutoAttackRangeFactor;
-		float MilitaryDefendRange;
-		int MilitaryRecentlyAttackedTime;
-		int MilitaryMaxAttackersPerEntity;
-		float CivilianDefenseRadius;
-		float MotivationThresholdHappy; // 69
-		float MotivationThresholdSad, MotivationThresholdAngry, MotivationThresholdLeave, AverageMotivationVillageCenterLockThreshold;
-		int WorkTimeBase, WorkTimeThresholdWork, WorkTimeThresholdFarm, WorkTimeThresholdResidence, WorkTimeThresholdCampFire;
-		float MotivationGameStartMaxMotivation;
-		float MotivationAbsoluteMaxMotivation; // 80
-		int MotivationMillisecondsWithoutJob;
-		int HeroResurrectionTime, HeroResurrectionSlippage;
-		float HeroResurrectionRadius, HeroResurrectionHealthFactor, HeroResurrectionActionPointFactor;
-		int HeroComatoseExploration;
-		int BattleSerfMaxSeconds;
-		shok::EffectTypeId ExpelEffectID; // 89
-		int DefenderMSPerShot; // 90
-		float DefenderMaxRange;
-		shok::EffectTypeId DefenderProjectileEffectType;
-		int DefenderProjectileDamage;
-		shok::DamageClassId DefenderProjectileDamageClass;
-		int DefenderMissChance; // 95
-		float WorkerFlightDistance;
-		int MaxExperiencePoints;
-		int BuildingRecentlyAttackedDuration;
-		float EnergyRequiredForWeatherChange; // 99
-		int DefaultDurationOfWeatherChange;
-		float EnergyDeductedFromOtherPlayers;
-		float ConstructionSiteHealthFactor;
-		float MaxFaith; // 103
-		float GuardMaxDistanceBuilding;
-		float GuardMoveDistanceBuilding;
-		float GuardMaxDistanceOther;
-		float GuardMoveDistanceOther;
-		float NPCInteractionDistance;
-		shok::AnimationId WeatherTowerAnim;
-		int LeaderNudgeCount;
-		float LeaderApproachRange;
-		int AlarmRechargeTime;
-		int OvertimeRechargeTimeInMs; // 113
+		int ResourceDoodadWarnAmount = 0; // 59
+		int FeedbackHandlerUpdateFrequency = 15; // 60
+		int FeedbackHandlerTimeStaysInSystem = 60;
+		float MilitaryLeaderAutoAttackRange = -1.0f;
+		float MilitaryBuildingAutoAttackRangeFactor = 0;
+		float MilitaryCivilianAutoAttackRangeFactor = 0;
+		float MilitaryDefendRange = 0;
+		int MilitaryRecentlyAttackedTime = 0;
+		int MilitaryMaxAttackersPerEntity = 0;
+		float CivilianDefenseRadius = 1.0f;
+		float MotivationThresholdHappy = 0; // 69
+		float MotivationThresholdSad = 0, MotivationThresholdAngry = 0, MotivationThresholdLeave = 0, AverageMotivationVillageCenterLockThreshold = 0;
+		int WorkTimeBase = 0, WorkTimeThresholdWork = 0, WorkTimeThresholdFarm = 0, WorkTimeThresholdResidence = 0, WorkTimeThresholdCampFire = 0;
+		float MotivationGameStartMaxMotivation = 0;
+		float MotivationAbsoluteMaxMotivation = 0; // 80
+		int MotivationMillisecondsWithoutJob = 0;
+		int HeroResurrectionTime = 1, HeroResurrectionSlippage = 0;
+		float HeroResurrectionRadius = 0, HeroResurrectionHealthFactor = 0, HeroResurrectionActionPointFactor = 0;
+		int HeroComatoseExploration = 0;
+		int BattleSerfMaxSeconds = 0;
+		shok::EffectTypeId ExpelEffectID = shok::EffectTypeId::Invalid; // 89
+		int DefenderMSPerShot = 0; // 90
+		float DefenderMaxRange = 0;
+		shok::EffectTypeId DefenderProjectileEffectType = shok::EffectTypeId::Invalid;
+		int DefenderProjectileDamage = 0;
+		shok::DamageClassId DefenderProjectileDamageClass = shok::DamageClassId::Invalid;
+		int DefenderMissChance = 0; // 95
+		float WorkerFlightDistance = 0;
+		int MaxExperiencePoints = 0;
+		int BuildingRecentlyAttackedDuration = 0;
+		float EnergyRequiredForWeatherChange = 0; // 99
+		int DefaultDurationOfWeatherChange = 0;
+		float EnergyDeductedFromOtherPlayers = 0;
+		float ConstructionSiteHealthFactor = 1.0f;
+		float MaxFaith = 0; // 103
+		float GuardMaxDistanceBuilding = 0;
+		float GuardMoveDistanceBuilding = 0;
+		float GuardMaxDistanceOther = 0;
+		float GuardMoveDistanceOther = 0;
+		float NPCInteractionDistance = 300.0f; // 108
+		shok::AnimationId WeatherTowerAnim = shok::AnimationId::Invalid;
+		int LeaderNudgeCount = 3;
+		float LeaderApproachRange = 200.0f;
+		int AlarmRechargeTime = 180000;
+		int OvertimeRechargeTimeInMs = 60000; // 113
 
 		static inline constexpr int vtp = 0x76EFCC;
 

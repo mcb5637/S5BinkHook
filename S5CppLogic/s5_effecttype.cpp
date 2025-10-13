86b2779ed930394607ffa4be280e38e74eda8a85
mcb <mgrbo@web.de>
2025-10-13 21:44:56 +0200

    fix fire effect props gain

--- S5CppLogic/s5_effecttype.cpp
+++ S5CppLogic/s5_effecttype.cpp
@@ -1,11 +1,12 @@
 #include "pch.h"
 #include "s5_effecttype.h"
+#include "s5_classfactory.h"
 #include "hooks.h"
 
 void __declspec(naked) fireprops_fix_ctor() {
 	__asm {
-		mov[esi + 0x2C], 0x41480000;
-		mov[esi + 48], 0;
+		mov dword ptr[esi + 0x2C], 0x41480000;
+		mov dword ptr[esi + 48], 0;
 
 		push 0x4FAC4D;
 		ret;
@@ -20,4 +21,9 @@ void GGL::CGLEffectFireProps::HookFixCtor()
 	CGLEffectFireProps_HookFixCtor = true;
 	CppLogic::Hooks::SaveVirtualProtect vp{ reinterpret_cast<void*>(0x4FAC46), 0x10 };
 	CppLogic::Hooks::WriteJump(reinterpret_cast<void*>(0x4FAC46), &fireprops_fix_ctor, reinterpret_cast<void*>(0x4FAC4D));
+	auto f = reinterpret_cast<BB::SerializationData * (__stdcall* const)()>(0x4FB0C6);
+	auto* sd = f();
+	if (sd[4].SerializationName == std::string_view("SoundID")) {
+		sd[4].SerializationName = "SoundIDAgain";
+	}
 }
